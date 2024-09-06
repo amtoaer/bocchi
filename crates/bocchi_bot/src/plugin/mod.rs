236672047f449b3a88142296439f491dcf81ed4c
ᴀᴍᴛᴏᴀᴇʀ <amtoaer@gmail.com>
2024-09-07 02:21:14 +0800

    refactor: 重构 bonus 插件的部分逻辑，避免相同逻辑出现多次 (#9)

--- crates/bocchi_bot/src/plugin/mod.rs
+++ crates/bocchi_bot/src/plugin/mod.rs
@@ -3,7 +3,7 @@ mod echo;
 mod gpt;
 mod repeat;
 
-pub use bonus::daily_bonus_plugin;
+pub use bonus::bonus_plugin;
 pub use echo::echo_plugin;
 pub use gpt::gpt_plugin;
 pub use repeat::repeat_plugin;
